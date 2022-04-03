8dcc5b43d360d985e958b66a5902216815644366
GalyautdinovIslam <galyautdinovislam@gmail.com>
2022-04-03 18:15:53 +0300

    create first models and config classes

---
+++ RadioActive-impl/src/main/java/ru/itis/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package ru.itis.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "ru.itis")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
